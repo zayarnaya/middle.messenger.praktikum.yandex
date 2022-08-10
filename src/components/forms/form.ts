669d2b7cef358bbbab01459a1fd4dd909cdd734b
Veronika Zayarnaya <veronika.zayarnaya@yandex.ru>
2022-08-10 22:01:02 +0300

    финальные правки

--- src/components/forms/form.ts
+++ src/components/forms/form.ts
@@ -2,8 +2,6 @@ import { Block } from "../../utils/block";
 import { submitError } from "../../utils/validator/submit-error";
 
 export class Form extends Block {
-
-
     constructor(formname: string, props: Object, tag?: string, classname?: string) {
         super(tag, props, false, classname);
         this.events = {
