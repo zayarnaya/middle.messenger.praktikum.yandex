2722b128798346bac078931b360a4c39599204c2
Veronika Zayarnaya <veronika.zayarnaya@yandex.ru>
2022-09-03 14:36:15 +0300

    добавлены тесты и конфигурационные файлы для них

---
+++ babel.config.js
@@ -0,0 +1,12 @@
+module.exports = (api) => {
+  // Cache configuration is a required option
+  api.cache(false);
+
+  const presets = [
+    '@babel/preset-typescript',
+    '@babel/preset-env',
+  ];
+
+  return { presets };
+};
+
--- src/test/babel-register.js
+++ src/test/babel-register.js
@@ -1,5 +1,9 @@
-//const register = require("@babel/register").default;
+const { JSDOM } = require('jsdom');
 
-const register = require("babel-register");
+const register = require('@babel/register').default;
 
-register({ extensions: ['.ts', '.tsx', '.js', '.jsx'] });
+register({ extensions: ['.ts', '.js'] });
+
+const dom = new JSDOM('<div class="messenger-wrapper"><div>', { url: 'http://localhost' });
+global.window = dom.window;
+global.document = dom.window.document;
