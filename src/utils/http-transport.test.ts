d4faf2c62f53a1bd43c00fd7f396695e94246f6d
Veronika Zayarnaya <veronika.zayarnaya@yandex.ru>
2022-09-07 15:10:15 +0300

    перенесен класс HTTPTransport для нужд тестирования

--- src/utils/http-transport.test.ts
+++ src/utils/http-transport.test.ts
@@ -1,6 +1,97 @@
-import { HTTPTransport } from "./http-transport";
+import XMLHttpRequest from "xhr2";
+import { Methods } from "../types";
 
-const request = new HTTPTransport();
+class HTTPTransport {
+  get = (url: string, options: Options) => {
+    return this.request(
+      !!options.data ? `${url}${queryStringify(options.data)}` : url,
+      { ...options, method: Methods.GET },
+      options.timeout
+    );
+  };
+
+  put = (url: string, options: Options) => {
+    return this.request(
+      url,
+      {
+        ...options,
+        headers: { "Content-Type": "application/json" },
+        method: Methods.PUT,
+      },
+      options.timeout
+    );
+  };
+
+  file = (url: string, options: Options) => {
+    return this.request(
+      url,
+      {
+        ...options,
+        method: Methods.PUT,
+      },
+      10000
+    );
+  };
+
+  post = (url: string, options: Options) => {
+    return this.request(
+      url,
+      {
+        ...options,
+        headers: { "Content-Type": "application/json" },
+        method: Methods.POST,
+      },
+      options.timeout
+    );
+  };
+
+  delete = (url: string, options: Options) => {
+    return this.request(
+      url,
+      { ...options, 
+        headers: { "Content-Type": "application/json" },
+        method: Methods.DELETE },
+      options.timeout
+    );
+  };
+
+  request = (url: string, options: Options, timeout: number = 1000) => {
+
+    const { headers = {}, method, data } = options;
+    return new Promise((resolve, reject) => {
+      if (!method) {
+        reject("no method!");
+        return;
+      }
+      const xhr = new XMLHttpRequest();
+      xhr.open(method, url);
+
+      Object.keys(headers).forEach((key) => {
+        xhr.setRequestHeader(key, headers[key]);
+      });
+
+      xhr.onload = function () {
+        resolve(xhr);
+      };
+
+      xhr.onabort = reject;
+      xhr.onerror = reject;
+      xhr.ontimeout = reject;
+
+      xhr.timeout = timeout;
+      xhr.withCredentials = true;
+
+      if (!data) {
+        xhr.send();
+      } else {
+        xhr.send(data);
+      }
+    });
+  };
+}
+
+
+const request = new HTTPTransport;
 //Используется fake API jsonplaceholder
 const prefix = "https://jsonplaceholder.typicode.com/posts";
 
@@ -83,3 +174,5 @@ describe("HTTPTransport", () => {
   });
 
 });
+
+
