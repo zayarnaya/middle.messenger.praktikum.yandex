ef587e2d4b543d6d5b646ad1732bb7b4ca26da1d
Veronika Zayarnaya <veronika.zayarnaya@yandex.ru>
2022-09-07 17:51:01 +0300

    изменения для автотестов

--- server.js
+++ server.js
@@ -12,14 +12,10 @@ app.use(
   })
 );
 
-app.use(express.static(path.join(__dirname, "dist")));
+app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get("/", (req, res) => {
-  res.sendFile(`${__dirname}/dist/index.html`);
-});
-
-app.get("/", (req, res) => {
-  res.sendFile(`${__dirname}/dist/index.html`);
+app.use('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
 app.listen(3000, () => {
