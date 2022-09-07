66a1e109d5c2832e480527311d8571b72dfd6161
Veronika Zayarnaya <veronika.zayarnaya@yandex.ru>
2022-09-07 18:48:53 +0300

    другая попытка пройти автотесты

--- server.js
+++ server.js
@@ -12,10 +12,14 @@ app.use(
   })
 );
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, "dist")));
 
-app.use('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(`${__dirname}/dist/index.html`);
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(`${__dirname}/dist/index.html`);
 });
 
 app.listen(3000, () => {
